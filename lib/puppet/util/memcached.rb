213e21f08b82c1da49b3089b087c131e3b365ea8
Luke Kanies <luke@puppetlabs.com>
2011-02-24 14:42:36 -0800

    Moving memcached support to a module
    Signed-off-by: Luke Kanies <luke@puppetlabs.com>

---
+++ lib/puppet/util/memcached.rb
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Requires 'dalli', but should be loaded via a 'feature', rather than
+# directly.  Currently has to be done by the including class.
+module Puppet::Util::Memcached
+  def memcache
+    # Compression in this client only hits when the value is 1K or larger, at
+    # which point it is probably a win for cache efficiency.  Reconsider this
+    # assumption if you use another client.
+    #
+    # Supply a default TTL for the data of an hour, plus a tiny bit, which
+    # should give a good efficiency vs "recover from strangeness" trade-off.
+    # Tune for your site, obviously, though the defaults should be
+    # sufficiently good...
+    @memcache ||= Dalli::Client.new('localhost:11211',
+                                    :compression => true,
+                                    :expires_in  => 62 * 60)
+  end
+
+  def memcache_key(request)
+    # We do our own namespacing, because we know more than the memcache client
+    # does about the different types of input we might have.
+    "puppet@#{request.indirection_name}@#{request.uri}"
+  end
+
+  def memcache_get(request)
+    key    = memcache_key(request)
+    p key
+    result = memcache.get(key)
+    p result
+
+    if result then
+      # We should probably assert the right document type, not just a known
+      # one.  Oh, well, this will do...
+      envelope = PSON.parse(result)
+      if decoder = PSON.registered_document_types[envelope['document_type']] then
+        return decoder.from_pson(envelope['data'])
+      end
+    end
+
+    Puppet.warning "past result testing"
+
+    # Couldn't decode it, or whatever.  We got here, so we want to get it,
+    # encode it, and store it before we return.
+    value = yield
+    store = value.to_pson
+    # key, value, TTL, options – raw means "don't mashall", which given that
+    # has a habit of both changing between versions, and corrupting random
+    # memory on invalid input, isn't really safe to use here.
+    memcache.set(key, value.to_pson, nil, :raw => true)
+
+    # Return that value, then.
+    value
+  end
+end
